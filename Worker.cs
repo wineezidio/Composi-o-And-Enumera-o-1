2095cf8286ad5e96d02335e83b594a160cb8c318
wineezidio <127625157+wineezidio@users.noreply.github.com>
2025-01-12 01:53:15 -0300

    Add files via upload

---
+++ Worker.cs
@@ -0,0 +1,51 @@
+﻿using Mid.Entities;
+using System.Collections.Generic;
+
+namespace Mid.Entities
+
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker () { }
+
+        public Worker (string name, WorkLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+
+        }
+
+        public void AddContract (HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract (HourContract contract) 
+        { 
+            Contracts.Remove(contract); 
+        }
+
+        public double Income (int yaer, int mont)
+        {
+            double sum = BaseSalary;
+
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == yaer && contract.Date.Month == mont)
+                {
+                    sum += contract.TotalVelue();
+
+                }
+            }
+            return sum;
+        }
+    }
+}
